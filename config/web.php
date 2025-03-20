52f257c31c378bfe27ff8a90c3b9452f6ca3ef7a
aldyryuza <agungaldi34@gmail.com>
2025-03-20 13:24:03 +0700

    konfigurasi id, name, timezone

--- config/web.php
+++ config/web.php
@@ -5,6 +5,8 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'timezone' => 'Asia/Jakarta',
+    'name' => 'Belajar Yii2 Basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
